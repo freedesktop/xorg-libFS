7e0de805849b4bb1951376600d152d0121b06b52
Egbert Eich <eich@suse.de>
2004-02-26 09:22:28 +0000

    Importing vendor version xf86-4_3_99_903 on Wed Feb 26 01:21:00 PST 2004

--- src/FSServName.c
+++ src/FSServName.c
@@ -24,7 +24,7 @@
  * ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS 
  * SOFTWARE.
  */
-/* $XFree86: xc/lib/FS/FSServName.c,v 1.4 2001/12/14 19:53:33 dawes Exp $ */
+/* $XFree86$ */
 
 /*
 
