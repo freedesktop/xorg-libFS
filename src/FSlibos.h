a31e5c7dd9db24cef779bad898fb2d3d4f84d6f3
Alexander Gottwald <alexander.gottwald@s1999.tu-chemnitz.de>
2004-09-15 16:33:02 +0000

    Pull XORG-6_8_0 to CYGWIN branch

--- src/FSlibos.h
+++ src/FSlibos.h
@@ -1,4 +1,4 @@
-/* $XdotOrg: xc/lib/FS/FSlibos.h,v 1.1.4.3.4.1 2004/03/04 17:46:25 eich Exp $ */
+/* $XdotOrg: xc/lib/FS/FSlibos.h,v 1.2 2004/04/23 18:43:08 eich Exp $ */
 /* $Xorg: FSlibos.h,v 1.4 2001/02/09 02:03:25 xorgcvs Exp $ */
 
 /* @(#)FSlibos.h	4.1	91/05/02
