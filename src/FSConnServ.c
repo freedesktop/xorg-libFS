905552dfa2af617780bc2cd4d14edad845c3203b
Keith Packard <keithp@keithp.com>
2005-07-13 07:23:56 +0000

    Make some minor source adjustments so that we don not need to use     -D_XOPEN_SOURCE -D_BSD_SOURCE everywhere as this will leak out of the     modular environment and into application build processes, potentially     foulling up their compiles.

--- src/FSConnServ.c
+++ src/FSConnServ.c
@@ -55,8 +55,8 @@ in this Software without prior written authorization from The Open Group.
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif
-#include	<stdio.h>
 #include	"FSlibint.h"
+#include	<stdio.h>
 #include	"X11/Xpoll.h"
 #ifdef NCD
 #include	<fcntl.h>
