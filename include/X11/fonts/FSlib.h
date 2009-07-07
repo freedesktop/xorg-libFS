233b72428a47272b599a6be0708b06f7ed674f7c
Alan Coopersmith <alan.coopersmith@sun.com>
2009-07-07 08:57:53 -0700

    Strip out-of-date RCS/CVS & SCCS version tags
    Signed-off-by: Alan Coopersmith <alan.coopersmith@sun.com>

--- include/X11/fonts/FSlib.h
+++ include/X11/fonts/FSlib.h
@@ -1,5 +1,3 @@
-/* $Xorg: FSlib.h,v 1.5 2001/02/09 02:03:25 xorgcvs Exp $ */
-
 /*
  * Copyright 1990 Network Computing Devices;
  * Portions Copyright 1987 by Digital Equipment Corporation
@@ -24,7 +22,6 @@
  * ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS 
  * SOFTWARE.
  */
-/* $XFree86: xc/lib/FS/FSlib.h,v 1.6 2001/12/14 19:53:33 dawes Exp $ */
 
 /*
 
